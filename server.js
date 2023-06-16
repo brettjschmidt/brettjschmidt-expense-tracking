ab6213b6e6da3815f7952f1f8873abaa7a58ae3b
brettjschmidt <brettjschmidt@gmail.com>
2023-06-16 14:17:06 -0500

    connected to db

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-require("dotenv").config()
+require("dotenv").config();
 
 const express = require("express");
 
-
 // express app
 const app = express();
-const balancesRoutes = require("./routes/balances")
+const mongoose = require("mongoose");
+const balancesRoutes = require("./routes/balances");
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
-
+    console.log(req.path, req.method);
+    next();
+});
 
 // routes
-app.use("/api/balances", balancesRoutes)
+app.use("/api/balances", balancesRoutes);
 
-
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("listening on port", process.env.PORT)
-})
+// connect to mongo
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("connected to Mongodb & listening on port", process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
